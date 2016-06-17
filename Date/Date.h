555edda836c55a4a8891dc9d672e13bfe49f4e11
David Castrejon <dcastrejon@Davids-MacBook-Pro.local>
2016-06-17 14:15:13 -0700

    Going to Branch for Date Development

--- Date/Date.h
+++ Date/Date.h
@@ -1,11 +1,23 @@
 #ifndef MYDATE_H
 #define MYDATE_H
+
+#include <string>
+#include <fstream>
+using namespace std;
+
+enum RelationType {LESS, EQUAL, GREATER};
+
 namespace MyDate {
 
 class Date{
+public:
+  void Initialize(int mVal, int dVal, int yVal);
   
-  
-  
+
+private:
+  int month;
+  int day;
+  int year;
 };
 
 }
